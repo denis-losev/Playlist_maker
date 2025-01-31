23ff7982b02da083683177dd7615a00673e89095
Denis Losev <xden15ka359x@yandex.ru>
2025-01-31 13:58:23 +0300

    Sprint 8: проведена работа над замечаниями

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -5,28 +5,34 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val searchButton = findViewById<Button>(R.id.search_button)
         val mediaLibraryButton = findViewById<Button>(R.id.media_button)
         val settingsButton = findViewById<Button>(R.id.settings_button)
 
-        searchButton.setOnClickListener{
+        searchButton.setOnClickListener {
             val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
             startActivity(displayIntent)
         }
 
-        mediaLibraryButton.setOnClickListener{
+        mediaLibraryButton.setOnClickListener {
             val displayIntent = Intent(this@MainActivity, MediaActivity::class.java)
             startActivity(displayIntent)
         }
 
-        settingsButton.setOnClickListener{
+        settingsButton.setOnClickListener {
             val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,18 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<ImageButton>(R.id.back_button)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
-        backButton.setOnClickListener{
-            val displayIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(displayIntent)
+        toolbar.setNavigationOnClickListener {
+            finish()
         }
     }
 }
