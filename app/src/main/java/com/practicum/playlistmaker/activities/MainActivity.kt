8b4b0e32f27e4fe3b09f4b13eb8ab7b51a0fa703
Denis Losev <xden15ka359x@yandex.ru>
2025-03-03 20:30:40 +0300

    Sprint 11: Сдаём задачу спринта на ревью

--- app/src/main/java/com/practicum/playlistmaker/activities/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.practicum.playlistmaker
+package com.practicum.playlistmaker.activities
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,6 +6,8 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.activities.search.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/practicum/playlistmaker/activities/search/SearchActivity.kt
@@ -0,0 +1,180 @@
+package com.practicum.playlistmaker.activities.search
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import retrofit2.converter.gson.GsonConverterFactory
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.iTunesApi.ITunesApi
+import com.practicum.playlistmaker.iTunesApi.TrackResponse
+import com.practicum.playlistmaker.track.Track
+import com.practicum.playlistmaker.track.TrackAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+class SearchActivity : AppCompatActivity() {
+
+    private val tracks = ArrayList<Track>()
+    private var searchValue: String = EMPTY_STRING
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchBarInput: EditText
+    private lateinit var errorContainer: LinearLayout
+    private lateinit var errorEmoji: ImageView
+    private lateinit var errorMessage: TextView
+    private lateinit var refreshButton: Button
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(ITunesApi::class.java)
+    private val adapter = TrackAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        recyclerView = findViewById(R.id.tracks_list)
+        searchBarInput = findViewById(R.id.search_bar)
+        errorContainer = findViewById(R.id.error_container)
+        errorEmoji = findViewById(R.id.error_emoji)
+        errorMessage = findViewById(R.id.error_message)
+        refreshButton = findViewById(R.id.refresh)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        refreshButton.setOnClickListener {
+            searchRequest()
+        }
+
+        setClearTextButton()
+        searchButtonListener()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_VALUE, searchValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchBarInput.setText(savedInstanceState.getString(SEARCH_VALUE))
+    }
+
+    private fun searchButtonListener() {
+
+        searchBarInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                searchRequest()
+                true
+            }
+            false
+        }
+    }
+
+    private fun searchRequest() {
+        iTunesService.search(searchBarInput.text.toString())
+            .enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        tracks.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            tracks.addAll(response.body()?.results!!)
+                            adapter.notifyDataSetChanged()
+                            showSearchResult(SearchResult.SUCCESS)
+                        } else if (response.body()?.results?.isEmpty() == true) {
+                            showSearchResult(SearchResult.NOT_FOUND)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, response: Throwable) {
+                    showSearchResult(SearchResult.NO_INTERNET_CONNECTION)
+                }
+
+            })
+    }
+
+    private fun setClearTextButton() {
+        val clearFieldButton = findViewById<ImageView>(R.id.clear_text)
+
+        clearFieldButton.isVisible = false
+
+        searchBarInput.setOnFocusChangeListener { view, b ->
+            if (b.not()) {
+                val inputService =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputService.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+        }
+
+        searchBarInput.doOnTextChanged { text, start, before, count ->
+            if (text.isNullOrEmpty()) {
+                clearFieldButton.isVisible = false
+            } else {
+                clearFieldButton.isVisible = true
+                searchValue = text.toString()
+            }
+        }
+
+        clearFieldButton.setOnClickListener {
+            recyclerView.visibility = View.GONE
+            searchBarInput.setText("")
+            searchBarInput.clearFocus()
+        }
+    }
+
+    private fun showSearchResult(state: SearchResult) {
+        when (state) {
+            SearchResult.SUCCESS -> {
+                errorContainer.visibility = View.GONE
+                refreshButton.visibility = View.GONE
+                adapter.tracks = tracks
+                recyclerView.adapter = adapter
+                recyclerView.visibility = View.VISIBLE
+            }
+
+            SearchResult.NOT_FOUND -> {
+                recyclerView.visibility = View.GONE
+                refreshButton.visibility = View.GONE
+                errorContainer.visibility = View.VISIBLE
+                errorEmoji.setImageResource(R.drawable.error404emoji)
+                errorMessage.setText(R.string.not_found_error_text)
+            }
+
+            SearchResult.NO_INTERNET_CONNECTION -> {
+                recyclerView.visibility = View.GONE
+                errorContainer.visibility = View.VISIBLE
+                errorEmoji.setImageResource(R.drawable.interneterroremoji)
+                errorMessage.setText(R.string.internet_error_text)
+                refreshButton.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    companion object {
+        const val EMPTY_STRING = ""
+        const val SEARCH_VALUE = "SEARCH_VALUE"
+        const val BASE_URL = "https://itunes.apple.com"
+    }
+}
